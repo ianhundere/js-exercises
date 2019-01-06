2863423a0187c4211d107fa7ec91dfbaab294fa9
Ian Hundere <ianfundere@gmail.com>
2019-01-06 08:08:58 -0500

    adds images and edits from practice projects

--- functions:arrays/evennum.js
+++ functions:arrays/evennum.js
@@ -1,10 +1,12 @@
-const arrayNums = [-1, 2, 6, -9, 4, -3];
+const arrayNums = [ -1, 2, 6, -9, 4, -3 ];
 let evenNums = [];
 
-arrayNums.forEach((arr) => {      // an annonymous function is created with the forEach method that checks if  
-    if (arr % 2 === 0) {         // each number in the array, arrayNums, is divisble by 2. if the number is divisible by 2
-        evenNums.push(arr);     // the number is appended to the new array, evenNums, using the push method.
-    }
+arrayNums.forEach((arr) => {
+	// an anonymous function is created with the forEach method that checks if
+	if (arr % 2 === 0) {
+		// each number in the array, arrayNums, is divisble by 2. if the number is divisible by 2
+		evenNums.push(arr); // the number is appended to the new array, evenNums, using the push method.
+	}
 });
 
-console.log(evenNums)
+console.log(evenNums);
--- functions:arrays/ntimes.js
+++ functions:arrays/ntimes.js
@@ -1,11 +1,14 @@
-function callNTimes(times, fun){            // a function is defined that takes two arguments, a number and a function.
-    for (let i = 0; i < times; i++) {       // a for loop is used to iterate through the number given, times, and then 
-        fun();                              // calls that function that many times.
-    }
+function callNTimes(times, fun) {
+	// a function is defined that takes two arguments, a number and a function.
+	for (let i = 0; i < times; i++) {
+		// a for loop is used to iterate through the number given, times, and then
+		fun(); // calls that function that many times.
+	}
 }
 
-function fun() {                            // the function that is called n times by the above function, callNTimes.
-    console.log(`Hello World!\n`);
+function fun() {
+	// the function that is called n times by the above function, callNTimes.
+	console.log(`Hello World!\n`);
 }
 
 callNTimes(10, fun);
